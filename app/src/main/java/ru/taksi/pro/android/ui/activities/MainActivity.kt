424bfb1d174fa45d64d8babfd723391f70adde6b
badmitry <dmitrij88@bk.ru>
2021-04-24 10:12:51 +0300

    ыаыва

--- app/src/main/java/ru/taksi/pro/android/ui/activities/MainActivity.kt
+++ app/src/main/java/ru/taksi/pro/android/ui/activities/MainActivity.kt
@@ -1,17 +1,12 @@
-package ru.taksi.pro.android.ui.activities
+package ru.taksi.pro.android.domain.activities
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.AbsoluteSizeSpan
-import android.text.style.ForegroundColorSpan
-import android.text.style.StyleSpan
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import ru.taksi.pro.android.R
 import ru.taksi.pro.android.databinding.ActivityMainBinding
+import ru.taksi.pro.android.domain.helpers.SpannableHelper
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,18 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val spannableTaxi = SpannableStringBuilder("Такси Про")
-        spannableTaxi.setSpan(
-            ForegroundColorSpan(Color.rgb(251,187,5)),
-            0, // start
-            1, // end
-            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
-        )
-        spannableTaxi.setSpan(AbsoluteSizeSpan(60), 0,1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableTaxi.setSpan(AbsoluteSizeSpan(60), 6,7, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableTaxi.setSpan(StyleSpan(android.graphics.Typeface.BOLD), 0,1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        binding.headerTitle.text = spannableTaxi
+        binding.headerTitle.text = SpannableHelper.spannableTaxi(getString(R.string.taxi_pro))
 
         binding.button3.setOnClickListener {
             startActivity(Intent(this, RegistrationActivity::class.java))
--- app/src/main/java/ru/taksi/pro/android/ui/activities/RegistrationActivity.kt
+++ app/src/main/java/ru/taksi/pro/android/ui/activities/RegistrationActivity.kt
@@ -1,23 +1,12 @@
-package ru.taksi.pro.android.ui.activities
+package ru.taksi.pro.android.domain.activities
 
-import android.graphics.Color
-import android.graphics.Typeface
-import android.os.Build
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.AbsoluteSizeSpan
-import android.text.style.ForegroundColorSpan
-import android.text.style.StyleSpan
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.bold
 import androidx.databinding.DataBindingUtil
 import ru.taksi.pro.android.R
 import ru.taksi.pro.android.databinding.RegistrMainBinding
-import ru.taksi.pro.android.ui.fragments.RegistrFragment
-import ru.taksi.pro.android.ui.fragments.RegistrFragmentEnterCode
-import ru.taksi.pro.android.ui.fragments.RegistrFragmentWelcome
+import ru.taksi.pro.android.domain.fragments.RegistrationFragment
+import ru.taksi.pro.android.domain.helpers.SpannableHelper
 
 class RegistrationActivity : AppCompatActivity() {
     lateinit var binding: RegistrMainBinding
@@ -25,28 +14,15 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.registr_main)
-        spannableTaxi(getString(R.string.taxi_pro))
+        binding.headerTitle.text = SpannableHelper.spannableTaxi(getString(R.string.taxi_pro))
     }
 
     override fun onStart() {
         super.onStart()
         supportFragmentManager.beginTransaction()
-            .add(R.id.container, RegistrFragment())
- //           .add(R.id.container, RegistrFragmentEnterCode())
- //           .add(R.id.container, RegistrFragmentWelcome())
+            .add(R.id.container, RegistrationFragment())
+            //           .add(R.id.container,RegistrFragmentEnterCode())
+//            .add(R.id.container, RegistrFragmentWelcome())
             .commit()
-    }
-    fun spannableTaxi(word: String) {
-        val spannableTaxi = SpannableStringBuilder(word)
-        spannableTaxi.setSpan(
-            ForegroundColorSpan(Color.rgb(251,187,5)),
-                0, // start
-                1, // end
-                Spannable.SPAN_EXCLUSIVE_INCLUSIVE,
-            )
-        spannableTaxi.setSpan(AbsoluteSizeSpan(60), 0,1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableTaxi.setSpan(AbsoluteSizeSpan(60), 6,7, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        spannableTaxi.setSpan(StyleSpan(Typeface.BOLD), 0,1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        binding.headerTitle.text = spannableTaxi
     }
 }
