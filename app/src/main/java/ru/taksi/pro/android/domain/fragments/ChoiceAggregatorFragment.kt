4731deed87ab31ff6322e82745213ec929a1ab13
Developer <elov62@gmail.com>
2021-05-01 01:35:47 +0500

    экран Мои данные - добавил Мой тариф и автомобиль

--- app/src/main/java/ru/taksi/pro/android/domain/fragments/ChoiceAggregatorFragment.kt
+++ app/src/main/java/ru/taksi/pro/android/domain/fragments/ChoiceAggregatorFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import ru.taksi.pro.android.R
 import ru.taksi.pro.android.databinding.ChoiceAggregatorFragmentBinding
-
 
 class ChoiceAggregatorFragment: Fragment() {
 private var binding: ChoiceAggregatorFragmentBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View = ChoiceAggregatorFragmentBinding.inflate(inflater, container, false).let {
-        binding = it
-        it.root
+    ): View? { binding = DataBindingUtil.inflate(inflater, R.layout.choice_aggregator_fragment, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
