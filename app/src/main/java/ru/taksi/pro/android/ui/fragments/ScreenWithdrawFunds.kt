78f4ad92575936e9d924138744700ce92919b887
IldusHisamutdinov <di3883id@yandex.ru>
2021-05-24 19:42:14 +0500

    синхрон сложения Вывод средств

--- app/src/main/java/ru/taksi/pro/android/ui/fragments/ScreenWithdrawFunds.kt
+++ app/src/main/java/ru/taksi/pro/android/ui/fragments/ScreenWithdrawFunds.kt
@@ -1,10 +1,13 @@
 package ru.taksi.pro.android.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import ru.taksi.pro.android.R
@@ -30,16 +33,35 @@ class ScreenWithdrawFunds : Fragment() {
         binding.run {
             myAggregatorYndexTaxi.visibility = View.GONE // не показывать агрегатор яндекс такси
             myAggregatorUber.visibility = View.GONE // не показывать агрегатор Uber
-            textTotalAmount.setOnClickListener {
-                totalAmount()
-            }
             buttonArrowRight.setOnClickListener { myBalance() } // назад в баланс
             bankCardDell1.setOnClickListener { dellBankCard1() } // удалить банк. карту 1
             bankCardDell2.setOnClickListener { dellBankCard2() } // удалить банк. карту 2
+            val gett = inputGett
+            val citimobile = inputCitimobile
+            val yndexTaxi = inputYndexTaxi
+            val uber = inputUber
+            rec(gett)
+            rec(citimobile)
+            rec(yndexTaxi)
+            rec(uber)
         }
     }
 
+    fun rec(agregator: EditText) {
+        agregator.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                // Прописываем то, что надо выполнить после изменения текста
+                binding.totalAmount.text = s.toString()
+                totalAmount()
+            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+        })
+    }
+
     fun totalAmount() {
+        var sum = 0
         var a = 0
         var b = 0
         var c = 0
@@ -68,7 +90,8 @@ class ScreenWithdrawFunds : Fragment() {
         } else {
             d = 0
         }
-        val sum = a + b + c + d
+        val list = listOf<Int>(a, b, c, d)
+        sum = list.sum()
         binding.totalAmount.text = sum.toString()
     }
 
